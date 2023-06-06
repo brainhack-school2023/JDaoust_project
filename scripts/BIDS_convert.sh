30990ecccef6a7d4ff16ec3a83a970b3951b0dbb
JustineD <63309940+JustineD56@users.noreply.github.com>
2023-06-06 16:14:24 -0400

    Update BIDS_convert.sh
    corrections raw_BIDS

--- scripts/BIDS_convert.sh
+++ scripts/BIDS_convert.sh
@@ -20,46 +20,46 @@ for file in ../data/raw/*; do  #find the session number
     session=4
   elif [[ ${name:6} = 'flw12' ]]; then
     session=3
-  elif [[ ${name:6} = 'flw2' && ${name::3} = 'RGC' ]]; then  #RGC participant had a different timeline
+  elif [[ ${name:6} = 'flw2' && ${name::3} = 'RGC' ]]; then  #RGC's participant had a different timeline
     session=x
   elif [[ ${name:6} = '' ]]; then 
     session=1
   else session=2
   fi
   cd $HOME
-  mkdir JDaoust_project/data/raw_BIDS/sub-${name::6}/ses-${session}/{anat,func} -p
-  dcm2niix -o ~/JDaoust_project/data/raw_BIDS/sub-${name::6}/ses-${session}/ -b y -m y -z y -f %i_%s_%p ~/JDaoust_project/data/raw/${name}/DICOM/
+  mkdir JDaoust_project/data/BIDS/sub-${name::6}/ses-${session}/{anat,func} -p
+  dcm2niix -o ~/JDaoust_project/data/BIDS/sub-${name::6}/ses-${session}/ -b y -m y -z y -f %i_%s_%p ~/JDaoust_project/data/raw/${name}/DICOM/
   
   #T1w
-  scp ~/projects/def-amichaud/share/dagher6_back_up/raw/${name}/DICOM/mcverter/*T1W*_orient.nii.gz ~/JDaoust_project/data/raw_BIDS/sub-${name::6}/ses-${session}/anat/sub-${name::6}_ses-${session}_T1w.nii.gz
-  scp ~/projects/def-amichaud/share/dagher6_back_up/raw/${name}/DICOM/mcverter/*T1W*_orient.nii ~/JDaoust_project/data/raw_BIDS/sub-${name::6}/ses-${session}/anat/sub-${name::6}_ses-${session}_T1w.nii
-  gzip ~/JDaoust_project/data/raw_BIDS/sub-${name::6}/ses-${session}/anat/sub-${name::6}_ses-${session}_T1w.nii
-  mv ~/JDaoust_project/data/raw_BIDS/sub-${name::6}/ses-${session}/*T1W*.json ~/JDaoust_project/data/raw_BIDS/sub-${name::6}/ses-${session}/anat/sub-${name::6}_ses-${session}_T1w.json
+  scp ~/projects/def-amichaud/share/dagher6_back_up/raw/${name}/DICOM/mcverter/*T1W*_orient.nii.gz ~/JDaoust_project/data/BIDS/sub-${name::6}/ses-${session}/anat/sub-${name::6}_ses-${session}_T1w.nii.gz
+  scp ~/projects/def-amichaud/share/dagher6_back_up/raw/${name}/DICOM/mcverter/*T1W*_orient.nii ~/JDaoust_project/data/BIDS/sub-${name::6}/ses-${session}/anat/sub-${name::6}_ses-${session}_T1w.nii
+  gzip ~/JDaoust_project/data/BIDS/sub-${name::6}/ses-${session}/anat/sub-${name::6}_ses-${session}_T1w.nii
+  mv ~/JDaoust_project/data/BIDS/sub-${name::6}/ses-${session}/*T1W*.json ~/JDaoust_project/data/BIDS/sub-${name::6}/ses-${session}/anat/sub-${name::6}_ses-${session}_T1w.json
   
   #BDM-run1
-  scp ~/projects/def-amichaud/share/dagher6_back_up/raw/${name}/DICOM/mcverter/*FOOD1_orient_cut.nii.gz ~/JDaoust_project/data/raw_BIDS/sub-${name::6}/ses-${session}/func/sub-${name::6}_ses-${session}_task-BDM_run-1_bold.nii.gz
-  scp ~/projects/def-amichaud/share/dagher6_back_up/raw/${name}/DICOM/mcverter/*FOOD1_orient_cut.nii ~/JDaoust_project/data/raw_BIDS/sub-${name::6}/ses-${session}/func/sub-${name::6}_ses-${session}_task-BDM_run-1_bold.nii
-  gzip ~/JDaoust_project/data/raw_BIDS/sub-${name::6}/ses-${session}/func/sub-${name::6}_ses-${session}_task-BDM_run-1_bold.nii
-  mv ~/JDaoust_project/data/raw_BIDS/sub-${name::6}/ses-${session}/*FOOD1*.json ~/JDaoust_project/data/raw_BIDS/sub-${name::6}/ses-${session}/func/sub-${name::6}_ses-${session}_task-BDM_run-1_bold.json
+  scp ~/projects/def-amichaud/share/dagher6_back_up/raw/${name}/DICOM/mcverter/*FOOD1_orient_cut.nii.gz ~/JDaoust_project/data/BIDS/sub-${name::6}/ses-${session}/func/sub-${name::6}_ses-${session}_task-BDM_run-1_bold.nii.gz
+  scp ~/projects/def-amichaud/share/dagher6_back_up/raw/${name}/DICOM/mcverter/*FOOD1_orient_cut.nii ~/JDaoust_project/data/BIDS/sub-${name::6}/ses-${session}/func/sub-${name::6}_ses-${session}_task-BDM_run-1_bold.nii
+  gzip ~/JDaoust_project/data/BIDS/sub-${name::6}/ses-${session}/func/sub-${name::6}_ses-${session}_task-BDM_run-1_bold.nii
+  mv ~/JDaoust_project/data/BIDS/sub-${name::6}/ses-${session}/*FOOD1*.json ~/JDaoust_project/data/BIDS/sub-${name::6}/ses-${session}/func/sub-${name::6}_ses-${session}_task-BDM_run-1_bold.json
   
   #BDM-run2
-  scp ~/projects/def-amichaud/share/dagher6_back_up/raw/${name}/DICOM/mcverter/*FOOD2_orient_cut.nii.gz ~/JDaoust_project/data/raw_BIDS/sub-${name::6}/ses-${session}/func/sub-${name::6}_ses-${session}_task-BDM_run-2_bold.nii.gz
-  scp ~/projects/def-amichaud/share/dagher6_back_up/raw/${name}/DICOM/mcverter/*FOOD2_orient_cut.nii ~/JDaoust_project/data/raw_BIDS/sub-${name::6}/ses-${session}/func/sub-${name::6}_ses-${session}_task-BDM_run-2_bold.nii
-  gzip ~/JDaoust_project/data/raw_BIDS/sub-${name::6}/ses-${session}/func/sub-${name::6}_ses-${session}_task-BDM_run-2_bold.nii
-  mv ~/JDaoust_project/data/raw_BIDS/sub-${name::6}/ses-${session}/*FOOD2*.json ~/JDaoust_project/data/raw_BIDS/sub-${name::6}/ses-${session}/func/sub-${name::6}_ses-${session}_task-BDM_run-2_bold.json
+  scp ~/projects/def-amichaud/share/dagher6_back_up/raw/${name}/DICOM/mcverter/*FOOD2_orient_cut.nii.gz ~/JDaoust_project/data/BIDS/sub-${name::6}/ses-${session}/func/sub-${name::6}_ses-${session}_task-BDM_run-2_bold.nii.gz
+  scp ~/projects/def-amichaud/share/dagher6_back_up/raw/${name}/DICOM/mcverter/*FOOD2_orient_cut.nii ~/JDaoust_project/data/BIDS/sub-${name::6}/ses-${session}/func/sub-${name::6}_ses-${session}_task-BDM_run-2_bold.nii
+  gzip ~/JDaoust_project/data/BIDS/sub-${name::6}/ses-${session}/func/sub-${name::6}_ses-${session}_task-BDM_run-2_bold.nii
+  mv ~/JDaoust_project/data/BIDS/sub-${name::6}/ses-${session}/*FOOD2*.json ~/JDaoust_project/data/BIDS/sub-${name::6}/ses-${session}/func/sub-${name::6}_ses-${session}_task-BDM_run-2_bold.json
   
   #BDM-run3
-  scp ~/projects/def-amichaud/share/dagher6_back_up/raw/${name}/DICOM/mcverter/*FOOD3_orient_cut.nii.gz ~/JDaoust_project/data/raw_BIDS/sub-${name::6}/ses-${session}/func/sub-${name::6}_ses-${session}_task-BDM_run-3_bold.nii.gz
-  scp ~/projects/def-amichaud/share/dagher6_back_up/raw/${name}/DICOM/mcverter/*FOOD3_orient_cut.nii ~/JDaoust_project/data/raw_BIDS/sub-${name::6}/ses-${session}/func/sub-${name::6}_ses-${session}_task-BDM_run-3_bold.nii
-  gzip ~/JDaoust_project/data/raw_BIDS/sub-${name::6}/ses-${session}/func/sub-${name::6}_ses-${session}_task-BDM_run-3_bold.nii
-  mv ~/JDaoust_project/data/raw_BIDS/sub-${name::6}/ses-${session}/*FOOD3*.json ~/JDaoust_project/data/raw_BIDS/sub-${name::6}/ses-${session}/func/sub-${name::6}_ses-${session}_task-BDM_run-3_bold.json
+  scp ~/projects/def-amichaud/share/dagher6_back_up/raw/${name}/DICOM/mcverter/*FOOD3_orient_cut.nii.gz ~/JDaoust_project/data/BIDS/sub-${name::6}/ses-${session}/func/sub-${name::6}_ses-${session}_task-BDM_run-3_bold.nii.gz
+  scp ~/projects/def-amichaud/share/dagher6_back_up/raw/${name}/DICOM/mcverter/*FOOD3_orient_cut.nii ~/JDaoust_project/data/BIDS/sub-${name::6}/ses-${session}/func/sub-${name::6}_ses-${session}_task-BDM_run-3_bold.nii
+  gzip ~/JDaoust_project/data/BIDS/sub-${name::6}/ses-${session}/func/sub-${name::6}_ses-${session}_task-BDM_run-3_bold.nii
+  mv ~/JDaoust_project/data/BIDS/sub-${name::6}/ses-${session}/*FOOD3*.json ~/JDaoust_project/data/BIDS/sub-${name::6}/ses-${session}/func/sub-${name::6}_ses-${session}_task-BDM_run-3_bold.json
   
-  sed -i '$s/}/,\n"TaskName":"BDM"}/' ~/JDaoust_project/data/raw_BIDS/sub-${name::6}/ses-${session}/func/sub-${name::6}_ses-${session}_task-BDM_run*.json
-  #jq 'del(.ImageOrientationPatientDICOM)' ~/JDaoust_project/data/raw_BIDS/sub-${name::6}/ses-${session}/func/sub-${name::6}_ses-${session}_task-BDM_run*.json
-  #jq 'del(.ImageOrientationPatientDICOM)' ~/JDaoust_project/data/raw_BIDS/sub-${name::6}/ses-${session}/anat/sub-${name::6}_ses-${session}_T1w.json
+  sed -i '$s/}/,\n"TaskName":"BDM"}/' ~/JDaoust_project/data/BIDS/sub-${name::6}/ses-${session}/func/sub-${name::6}_ses-${session}_task-BDM_run*.json
+  #jq 'del(.ImageOrientationPatientDICOM)' ~/JDaoust_project/data/BIDS/sub-${name::6}/ses-${session}/func/sub-${name::6}_ses-${session}_task-BDM_run*.json
+  #jq 'del(.ImageOrientationPatientDICOM)' ~/JDaoust_project/data/BIDS/sub-${name::6}/ses-${session}/anat/sub-${name::6}_ses-${session}_T1w.json
 
-  rm ~/JDaoust_project/data/raw_BIDS/sub-${name::6}/ses-${session}/GUT*
-  rm ~/JDaoust_project/data/raw_BIDS/sub-${name::6}/ses-${session}/gut*
+  rm ~/JDaoust_project/data/BIDS/sub-${name::6}/ses-${session}/GUT*
+  rm ~/JDaoust_project/data/BIDS/sub-${name::6}/ses-${session}/gut*
   
   for run in 1 2 3; do
      paste ~/projects/def-amichaud/share/dagher6_back_up/EVs/bariatric_${name::6}_session${session}_run${run}_img_onset*.txt ~/projects/def-amichaud/share/dagher6_back_up/EVs/bariatric_${name::6}_session${session}_run${run}_img_duration*.txt ~/projects/def-amichaud/share/dagher6_back_up/EVs/bariatric_${name::6}_session${session}_run${run}_img_bids*.txt > ~/JDaoust_project/data/raw_BIDS/sub-${name::6}/ses-${session}/func/sub-${name::6}_ses-${session}_task-BDM_run-${run}_events.tsv
